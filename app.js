8f335371ed296c9d7626f6ae7eaec252cf67a597
Hassan <hasanahmad9812021@gmail.com>
2024-09-22 18:05:42 +0600

    CRUD

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Application Configuration
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const router = require("./src/routes/api");
